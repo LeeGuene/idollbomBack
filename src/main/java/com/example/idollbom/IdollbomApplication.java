2f431b52615143f61fe56a143a6007ab2caabf38
Junnybro <cyj10200@gmail.com>
2024-07-05 20:55:47 +0900

    아이돌봄 플젝 추가

---
+++ src/main/java/com/example/idollbom/IdollbomApplication.java
@@ -0,0 +1,13 @@
+package com.example.idollbom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdollbomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdollbomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/idollbom/IdollbomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.idollbom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdollbomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
