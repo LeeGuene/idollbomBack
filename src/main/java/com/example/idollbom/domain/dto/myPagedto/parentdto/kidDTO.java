dd9cba2edb4d3f34ff30619b261fc520dc6e0545
mjkang4416 <mjkang4416@naver.com>
2024-07-07 00:43:39 +0900

    [feat] kids페이지 이동

---
+++ src/main/java/com/example/idollbom/domain/dto/myPagedto/parentdto/kidDTO.java
@@ -0,0 +1,12 @@
+package com.example.idollbom.domain.dto.myPagedto.parentdto;
+
+import lombok.Data;
+
+@Data
+public class kidDTO {
+    private String childName;
+    private String childAge;
+    private String childGender;
+    private String childSpecialIssues;
+    private Long parentNumber;
+}
