d53637c97ee4096c4bb9f5d98c6e6f41280a3085
mjkang4416 <mjkang4416@naver.com>
2024-07-08 00:17:14 +0900

    [style]아이정보 추가 완성

--- src/main/resources/static/js/modal.js
+++ src/main/resources/static/js/modal.js
@@ -3,11 +3,21 @@ const modal2 = document.querySelector('.modal2');
 const btnOpenModal=document.querySelector('.btn-open-modal');
 const btnCloseModal=document.querySelector('.close-btnen');
 const btnCloseModal2=document.querySelector('.close-btnen2');
-const btnOpenModalUpdate =document.querySelector('.btn-update-modal')
+const closeInsertModal= document.querySelector('.cancle')
 
 
-btnOpenModalUpdate.addEventListener("click", ()=>{
-    modal2.style.display="flex";
+closeInsertModal.addEventListener("click", ()=>{
+    modal2.style.display="none";
+});
+
+// 모든 수정 버튼을 선택
+const btnOpenModalUpdates = document.querySelectorAll('.btn-update-modal');
+// 모달활성화
+btnOpenModalUpdates.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const childNumber = btn.getAttribute('data-kid-number');
+        modal2.style.display = "flex";
+    });
 });
 
 btnOpenModal.addEventListener("click", ()=>{
