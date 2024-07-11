f0689c9add9737bb92a9be4f2fca454df52d2cec
LeeGuene <tlsdlrms1@naver.com>
2024-07-11 17:19:26 +0900

    studyDetail, review html, css 수정 및 컨트롤러 추가

--- src/main/java/com/example/idollbom/domain/dto/parentdto/ReviewDTO.java
+++ src/main/java/com/example/idollbom/domain/dto/parentdto/ReviewDTO.java
@@ -3,6 +3,7 @@ package com.example.idollbom.domain.dto.parentdto;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
--- src/main/java/com/example/idollbom/domain/vo/reviewVO.java
+++ src/main/java/com/example/idollbom/domain/vo/reviewVO.java
@@ -2,29 +2,32 @@ package com.example.idollbom.domain.vo;
 
 import com.example.idollbom.domain.dto.parentdto.ReviewDTO;
 import lombok.*;
+import org.springframework.cglib.core.Local;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
 @ToString
 @NoArgsConstructor
 @Builder
-@AllArgsConstructor
 public class reviewVO {
-    private Long reviewNumber;              // pk
-    private String reviewContent;           // 리뷰 내용
-    private Long reviewEvaluationPoint;     // 별점
-    private Date reviewRegisterDate;        // 리뷰 등록일
-    private Date reviewUpdateDate;          // 리뷰 수정일
-    private Long parentNumber;              // fk (부모 번호)
-    private Long classNumber;               // fk (수업 번호)
+    private Long reviewNumber;                      // pk
+    private String reviewContent;                   // 리뷰 내용
+    private Long reviewEvaluationPoint;             // 별점
+    private LocalDate reviewRegisterDate;       // 리뷰 등록일
+    private LocalDate reviewUpdateDate;         // 리뷰 수정일
+    private Long parentNumber;                      // fk (부모 번호)
+    private Long classNumber;                       // fk (수업 번호)
 
     @Builder
-    public reviewVO(Long reviewNumber, String reviewContent, Long reviewEvaluationPoint, Date reviewRegisterDate, Long parentNumber, Long classNumber) {
+    public reviewVO(Long reviewNumber, String reviewContent, Long reviewEvaluationPoint, LocalDate reviewRegisterDate, LocalDate reviewUpdateDate, Long parentNumber, Long classNumber) {
         this.reviewNumber = reviewNumber;
         this.reviewContent = reviewContent;
         this.reviewEvaluationPoint = reviewEvaluationPoint;
         this.reviewRegisterDate = reviewRegisterDate;
+        this.reviewUpdateDate = reviewUpdateDate;
         this.parentNumber = parentNumber;
         this.classNumber = classNumber;
     }
