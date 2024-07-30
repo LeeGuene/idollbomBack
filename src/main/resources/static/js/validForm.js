c997fa277b6c5d847a2ce491de6a499f8397aa11
moeunseo <mes98@naver.com>
2024-07-30 20:58:38 +0900

    수업 등록 유효성 검사 진행 중

--- src/main/resources/static/js/validForm.js
+++ src/main/resources/static/js/validForm.js
@@ -1,51 +1,109 @@
+// 수업 제목
+const title = document.getElementById('title')
+// 해당 부모 노드로 찾아가는 parentNode
+const classTitle = document.getElementById('classTitle').parentNode
+
+// 수업 소개
+const intro = document.getElementById('intro')
+const classIntro = document.getElementById('classIntro').parentNode
+
+// 날짜 선택
+// 당일로 한다면, 등록하지 못하게 막기
+const dateSelect = document.getElementById('dateSelect')
+const classDate = document.getElementById('classDate').parentNode
+// 현재 날짜를 가져옵니다.
+const today = new Date()
+// 날짜의 연도, 월, 일을 추출합니다.
+const year = today.getFullYear()
+const month = String(today.getMonth() + 1).padStart(2, '0') // 월은 0부터 시작하므로 +1
+const day = String(today.getDate()).padStart(2, '0')
+// 형식에 맞게 날짜를 포맷합니다.
+const formattedDate = `${year}-${month}-${day}`
+
+// 시간 선택
+const timeSelect = document.getElementById('timeSelect')
+const classDay = document.getElementById('classDay').parentNode
+
+// 수업 사진
+const upload = document.getElementById('upload')
+const classImage = document.getElementById('classImage').parentNode
+
+// 수업 내용
+const content = document.getElementById('content')
+const classContent = document.getElementById('content').parentNode
+
+// 결제 금액
+const money = document.getElementById('payment-amount')
+const classMoney = document.getElementById('classMoney').parentNode
+
 // 유효성 체크
 function validForm() {
-    let isValid = true;
-    let message = "";
+    let isValid = true
 
-    console.log('실행되니?')
-
-    // 수업 제목
-    const title = document.getElementById('title').value
-    console.log(title === '')
-    // 해당 부모 노드로 찾아가는
-    const classTitle = document.getElementById('classTitle').parentNode
-    if (title === '') {
-        console.log(title === '')
+    if (title.value === '') {
         classTitle.style.display="flex"
-        isValid = false;
+        isValid = false
     }
 
-    // 수업 소개
-    const intro = document.getElementById('intro').value;
-    const classIntro = document.getElementById('classIntro')
-    if (!intro) {
-        classIntro.style.display="block"
-        isValid = false;
+    if (intro.value === '') {
+        classIntro.style.display="flex"
+        isValid = false
     }
 
-    // // 날짜 선택
-    // // 당일로 한다면, 등록하지 못하게 막기
-    // const dateSelect = document.getElementById('dateSelect').value;
-    // if (!dateSelect) {
-    //   message += "날짜를 선택해 주세요.\n";
-    //   isValid = false;
-    // }
-    //
-    // // 시간 선택
-    // const timeSelect = document.getElementById('timeSelect').value;
-    // if (timeSelect === "none") {
-    //   message += "시간을 선택해 주세요.\n";
-    //   isValid = false;
-    // }
-    //
-    // // 수업 사진
-    // const upload = document.getElementById('upload').files.length;
-    // if (upload === 0) {
-    //   message += "수업 사진을 업로드해 주세요.\n";
-    //   isValid = false;
-    // }
+    if (dateSelect.value === formattedDate) {
+        classDate.style.display="flex"
+        isValid = false
+    }
+
+    if (timeSelect.value === "none") {
+        classDay.style.display="flex"
+        isValid = false
+    }
+
+    if (upload.files.length === 0) {
+        classImage.style.display="flex"
+        isValid = false
+    }
+
+    if(content.value === ''){
+        classContent.style.display="flex"
+        isValid = false
+    }
+
+    if(money.value === '') {
+        classMoney.style.display = "flex"
+        isValid = false
+    }
 
     // 값이 true여야만 실행
     return isValid
 }
+
+
+// 수업 제목이 채워졌다면
+title.addEventListener("change", function(e){
+    classTitle.style.display = 'none'
+})
+
+// 수업 소개가 채워졌다면
+intro.addEventListener("change", function(e){
+    classIntro.style.display = 'none'
+})
+
+// 수업 날짜가 금일 날짜와 다르다면
+dateSelect.addEventListener("change", function(e){
+    classDate.style.display = 'none'
+})
+
+timeSelect.addEventListener("change", function(e){
+    classDay.style.display = 'none'
+})
+
+upload.addEventListener("change", function(e){
+    classImage.style.display = 'none'
+})
+
+money.addEventListener("change", function (){
+    classMoney.style.display= 'none'
+})
+
